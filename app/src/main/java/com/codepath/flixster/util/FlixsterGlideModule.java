cf5545d6ff4e5d4ba9180dc8adbd3e7f8210f45b
Jonathan Abantao <jonabantao@gmail.com>
2020-09-30 17:55:59 -0700

    Fix AppGlideModule warning

---
+++ app/src/main/java/com/codepath/flixster/util/FlixsterGlideModule.java
@@ -0,0 +1,9 @@
+package com.codepath.flixster.util;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class FlixsterGlideModule extends AppGlideModule {
+
+}
